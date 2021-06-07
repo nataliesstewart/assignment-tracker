f3f40e49618b1278c58e4de17dac8bd6ca86877b
Natalie Stewart <nataliesstewart16@gmail.com>
2021-06-07 12:08:36 -0400

    Initial commit

---
+++ newtab.js
@@ -0,0 +1,32 @@
+var text;
+
+document.body.onload = function() {
+  chrome.storage.sync.get("data", function(object) {
+    if (!chrome.runtime.error) text = object.data;
+    else text = "";
+    document.getElementById("textArea").value = text;
+  });
+
+  window.setInterval(function() {
+    if (text !== document.getElementById("textArea").value) {
+      saveText();
+      text = document.getElementById("textArea").value;
+    }
+  }, 3000);
+
+  chrome.storage.onChanged.addListener(function(changes, namespace) {
+    for (key in changes) {
+      var storageChange = changes[key];
+      document.getElementById("textArea").value = storageChange.newValue;
+    };
+  });
+};
+
+
+
+var saveText = function() {
+  text = document.getElementById("textArea").value;
+  chrome.storage.sync.set({ "data" : text }, function() {
+    if (chrome.runtime.error) console.log("Runtime error.");
+  });
+};
