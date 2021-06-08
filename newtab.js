55b0881833bfb8dcc4fad2c494c7a239d6d14003
Natalie Stewart <nataliesstewart16@gmail.com>
2021-06-07 22:58:50 -0400

    Added options dialog

--- newtab.js
+++ newtab.js
@@ -4,6 +4,16 @@ document.body.onload = function() {
   chrome.storage.sync.get("data", function(object) {
     if (!chrome.runtime.error) text = object.data;
     else text = "";
+    var backgroundColor;
+    var foregroundColor;
+    chrome.storage.sync.get("backgroundColor", function(items){
+      document.getElementById("html-body").style["backgroundColor"] = items.backgroundColor;
+      document.getElementById("body").style["backgroundColor"]      = items.backgroundColor;
+      document.getElementById("textArea").style["backgroundColor"]  = items.backgroundColor;
+      document.getElementById("html-body").style["foregroundColor"] = items.foregroundColor;
+      document.getElementById("body").style["foregroundColor"]      = items.foregroundColor;
+      document.getElementById("textArea").style["foregroundColor"]  = items.foregroundColor;
+    });
     document.getElementById("textArea").value = text;
   });
 
